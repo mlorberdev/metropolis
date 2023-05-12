22586b3254e03ff67d433521bfab9df1f5f6c85f
mlorber <87738509+mlorberdev@users.noreply.github.com>
2023-05-12 14:01:31 -0400

    set vars

---
+++ metropolis.js
@@ -0,0 +1,11 @@
+!(function () {
+  const seed = 10001; // make reproducible
+  let n; // iterations
+  let r; // neighborhood
+  let i = 3; // number of initial points
+  const f = 500; // number of tween frames
+  const d = 2 * Math.PI / 180; // angle direction noise
+  const p_branch = .1; // probability of branching
+  const w = .8 * innerWidth;
+  const h = .8 * innerHeight;
+})();
